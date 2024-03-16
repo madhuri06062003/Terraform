6816bbadb307115924b19ad904f4e70986442c7e
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2024-03-16 23:08:24 +0530

    2

---
+++ practice2/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    description = "passing"
+    type = string
+    default = ""
+}
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+variable "key_name" {
+    type = string
+    default = ""  
+}
