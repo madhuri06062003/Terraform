e0e4474f7fe17bfd71d1c248618fbc06647742b8
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2024-03-17 12:16:36 +0530

    2

---
+++ practice2/main.tf
@@ -0,0 +1,42 @@
+#create VPC
+resource "aws_vpc" "dev" {
+cidr_block = "10.0.0.0/16"
+tags={
+    Name = "dev"
+}  
+}
+#create internet gateway and attach to vpc
+resource "aws_internet_gateway" "dev" {
+    vpc_id = aws_vpc.dev.id #IG attach block to vpc
+  
+}
+
+#create subnets
+resource "aws_subnet" "dev" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.dev.id #subnet attach block yo vpc
+  
+}
+
+
+#cretae Route tables & attach internet gateway to Rt
+
+resource "aws_route_table" "dev" {
+    vpc_id = aws_vpc.dev.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev.id #attache block ig to rt
+     
+  }
+  
+}
+#subent associations
+resource "aws_route_table_association" "name" {
+    route_table_id = aws_route_table.dev.id #rout table association rt attach
+    subnet_id = aws_subnet.dev.id #rt association to subnet
+  
+}
+
+
+
+  
---
+++ practice2/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-07bff6261f14c3a45"
+instance_type = "t2.micro"
+key_name = "vpc"
